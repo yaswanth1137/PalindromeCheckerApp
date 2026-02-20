3809ca30eaabfe7e7d4a272ae9ccd9d477ed07b5
yaswanth1137 <yaswanth41210@gmail.com>
2026-02-20 11:05:03 +0530

    Update uc6.java

---
+++ uc6.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class uc6 {
+    public static void main(String[] args) {
+        String input = "racecar";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
