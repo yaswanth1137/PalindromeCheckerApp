09d3ff006f50186272fdc4ec7382dbb1b33c62c6
yaswanth1137 <yaswanth41210@gmail.com>
2026-02-20 10:27:14 +0530

    Initial commit of Palindrome Checker

---
+++ uc3.java
@@ -0,0 +1,19 @@
+public class UseCase3PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "madam";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + reversed);
+
+        if (input.equals(reversed)) {
+            System.out.println("Result: It is a Palindrome.");
+        } else {
+            System.out.println("Result: It is NOT a Palindrome.");
+        }
+    }
+}
---
+++ uc4.java
@@ -0,0 +1,22 @@
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "radar";
+        char[] chars = input.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
